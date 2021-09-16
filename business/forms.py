1e34e7cabbdaf0b7b239ce31433d656d6ce207ff
Emm Kwami <edenated@gmail.com>
2021-09-16 14:05:19 -0700

    correct model applied

--- business/forms.py
+++ business/forms.py
@@ -6,7 +6,7 @@ from .models import Business
 class BusinessProfileForm(ModelForm):
     class Meta:
         model = Business
-        fields = ['name','phone','email','bio','instagram','facebook','twitter','website','postcode','address1','address2','town','country']
+        fields = ['business_name','business_phone','business_email','business_bio','instagram','facebook','twitter','website','postcode','address_line_1','address_line_2','town','country']
     def __init__(self,*args,**kwargs):
         super(BusinessProfileForm,self).__init__(*args,**kwargs)
         for name, field in self.fields.items():
